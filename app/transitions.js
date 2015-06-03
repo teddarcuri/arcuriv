890746fbc2f529bb146547b0b9ca941b49463788
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-03 11:31:13 -0600

    Some transition improvements

--- app/transitions.js
+++ app/transitions.js
@@ -1,12 +1,32 @@
 export default function() {
 
+    let duration = 700;
+
     this.transition(
       this.fromRoute('about'),
-      this.use('toUp')
+      this.use('explode', {
+          pickOld: 'h1',
+          use: ['toLeft', {duration}],
+        }, {
+          pickNew: 'h1',
+          use: ['toRight', {duration}]
+        }, {
+          use: ['fade', {duration}]
+        }
+      )
     );
 
     this.transition(
       this.fromRoute('contact'),
-      this.use('toDown')
+      this.use('explode', {
+          pickOld: 'h1',
+          use: ['toLeft', {duration}],
+        }, {
+          pickNew: 'h1',
+          use: ['toRight', {duration}]
+        }, {
+          use: ['fade', {duration}]
+        }
+      )
     );
 };
